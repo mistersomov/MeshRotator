1b6a5eaac72281c9be147ce8e1d47d7aa18eebb2
Ivan Somov <msijoc@gmail.com>
2024-03-08 19:44:19 +0300

    Refactoring Shader.cpp

--- common/ndk_helper/src/main/cpp/Shader.cpp
+++ common/ndk_helper/src/main/cpp/Shader.cpp
@@ -16,7 +16,7 @@ void ndk_helper::shdr::Shader::set_float(const std::string& name, float value) c
 
 void ndk_helper::shdr::Shader::set_vec3(const std::string& name, const glm::vec3 value) const {
     glUniform3fv(
-        glGetUniformLocation(id_,name.c_str()),
+        glGetUniformLocation(id_, name.c_str()),
         1,
         glm::value_ptr(value)
     );
@@ -24,7 +24,7 @@ void ndk_helper::shdr::Shader::set_vec3(const std::string& name, const glm::vec3
 
 void ndk_helper::shdr::Shader::set_mat4(const std::string& name, const glm::mat4 value) const {
     glUniformMatrix4fv(
-        glGetUniformLocation(id_,name.c_str()),
+        glGetUniformLocation(id_, name.c_str()),
         1,
         GL_FALSE,
         glm::value_ptr(value)
--- common/ndk_helper/src/main/cpp/Shader.hpp
+++ common/ndk_helper/src/main/cpp/Shader.hpp
@@ -13,10 +13,10 @@ namespace ndk_helper {
             Shader(GLuint) noexcept;
 
             void activate() const;
-            void set_int(const std::string&, int) const;
-            void set_float(const std::string&, float) const;
-            void set_vec3(const std::string&, const glm::vec3) const;
-            void set_mat4(const std::string&, const glm::mat4) const;
+            void set_int(const std::string& name, int value) const;
+            void set_float(const std::string& name, float value) const;
+            void set_vec3(const std::string& name, const glm::vec3 value) const;
+            void set_mat4(const std::string& name, const glm::mat4 value) const;
 
             GLuint id_;
         };
